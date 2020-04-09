b8b6b7cced77fbc42a17c4ed8ed45024c7c2e885
Cesar Santos <cesar.henrique121@gmail.com>
2020-04-09 15:50:53 -0300

    Add Filas para Envio de emails

--- src/config/mail.js
+++ src/config/mail.js
@@ -3,8 +3,8 @@ export default {
   port: 2525,
   secure: false,
   auth: {
-    user: '',
-    pass: '',
+    user: 'd9687791657447',
+    pass: '1785d7603b69c8',
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobaber.com>',
