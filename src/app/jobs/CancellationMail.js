f49caab74efec6395618512a16d5ea26b25caf49
Cesar Santos <cesar.henrique121@gmail.com>
2020-04-10 18:49:36 -0300

    Metodo para captura erros na fila de processamento de email

--- src/app/jobs/CancellationMail.js
+++ src/app/jobs/CancellationMail.js
@@ -10,8 +10,6 @@ class CancellationMail {
   async handle({ data }) {
     const { appointment } = data;
 
-    console.log('Fila em andamento...');
-
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: `Agedamento cancelado`,
